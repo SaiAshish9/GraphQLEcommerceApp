a3f7dc7adf4017824446088037a71b5cd5fb73f4
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2019-11-12 01:28:47 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Switch,Route,Link} from 'react-router-dom'
 import ShopPage from './pages/shop/shop'
 
 import Header from './components/header/header'
+import SignIn from './pages/sign-in-and-sign-up/sign-in-and-sign-up'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
        <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route  path='/shop' component={ShopPage}/>
+        <Route  path='/signin' component={SignIn}/>
 
 
       </Switch>
