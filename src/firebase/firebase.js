8f8f9141ba46f9817231b0f44d792f0091cd7020
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2019-12-08 16:45:22 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,7 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const config= {
-  apiKey: "AIzaSyCGcIVOZoE2UZIIzGIis1hwM_CZLbJRVKc",
-  authDomain: "graphqldb.firebaseapp.com",
-  databaseURL: "https://graphqldb.firebaseio.com",
-  projectId: "graphqldb",
-  storageBucket: "graphqldb.appspot.com",
-  messagingSenderId: "226883476148",
-  appId: "1:226883476148:web:11e1118a809c91741695e1",
-  measurementId: "G-W83BNE3VKB"
+
 };
 
 
